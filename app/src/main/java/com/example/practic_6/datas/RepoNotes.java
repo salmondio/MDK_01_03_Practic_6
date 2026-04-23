c421e4c730f15a233c9be0ee56329b7434ddd5e0
salmondio <sergej.serzanin1@gmail.com>
2026-04-23 11:31:40 +0500

    разработана активность заметки, модель и репозиторий данных. сержанин

---
+++ app/src/main/java/com/example/practic_6/datas/RepoNotes.java
@@ -0,0 +1,10 @@
+package com.example.practic_6.datas;
+
+import com.example.practic_6.domains.models.Note;
+
+import java.util.ArrayList;
+
+
+public class RepoNotes {
+    public static ArrayList<Note> Notes = new ArrayList<>();
+}
---
+++ app/src/main/java/com/example/practic_6/domains/models/Note.java
@@ -0,0 +1,9 @@
+package com.example.practic_6.domains.models;
+
+public class Note {
+    public int id;
+    public String title;
+    public String text;
+    public String date;
+    public String color;
+}
